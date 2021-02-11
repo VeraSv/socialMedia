a8d2f89d8097de33fa32ecfdb681a77ba12de110
VeraSv <vera.svistun05@gmail.com>
2021-02-11 19:59:12 +0300

    create copy of state

--- src/dialogs-reducer.js
+++ src/dialogs-reducer.js
@@ -4,25 +4,19 @@ let initialState ={userData:[{id:1, name:"Slava",img:"https://im0-tub-by.yandex.
 messagesData:{data:[{id:1, message:"Hi"} ,{id:2, message:"How are you"},{id:3, message:"Have a good day!"},
 {id:4, message:"Be happy"}], newMessageBody:''}};
  const dialogsReducer =(state=initialState, action) =>{
-     let stateCopy={...state, messagesData:{...state.messagesData}};
-     stateCopy.messagesData={...state.messagesData};
+    // let stateCopy;
     switch (action.type) {
         
         case updateNewMessageBody:
-            stateCopy.messagesData.newMessageBody=action.newText;
+          return {...state, messagesData:{...state.messagesData, newMessageBody:action.newText}};
                 
-                break;
         case sendMessage:
-            let body= state.messagesData.newMessageBody;
-
-            stateCopy.messagesData.data.push({id:6, message:body});
-           stateCopy.messagesData.newMessageBody='';
-        break;
+            return {...state, messagesData:{...state.messagesData, newMessageBody:'',data:[...state.messagesData.data,{id:6, message:state.messagesData.newMessageBody}]}};
+      
         default: 
-return stateCopy;
+return state;
     }
     
-    return stateCopy;
 }
 
 export default dialogsReducer
--- src/profile-reducer.js
+++ src/profile-reducer.js
@@ -3,31 +3,20 @@ const updateNewPostText='updateNewPostText';
 
 let initialState = {data:[{id:1, message:"Hi!", likesCount:"5"}, {id:2, message:"How are you?", likesCount:"0"}], newPostText:''};
  const profileReducer =(state=initialState, action) => {
-    let stateCopy= {...state};
-    stateCopy.data=[...state.data];
+
     switch (action.type) {
       
         case addPost:
-            let newPost={
-                id:3,
-                message:state.newPostText,
-                likesCount:0
-            }
-            
-        stateCopy.data.push(newPost);
-           stateCopy.newPostText='';
-           
-            break;
+                      
+        return {...state,newPostText:'',data:[...state.data, { id:3,message:state.newPostText,
+            likesCount:0}]};
+
         case updateNewPostText:
-           
-          stateCopy.newPostText=action.newText;
-           
-            break;
+           return {...state, newPostText:action.newText};
         
         default: 
-   return stateCopy;
+   return state;
 }
-return stateCopy;
 }
 
 export default profileReducer
